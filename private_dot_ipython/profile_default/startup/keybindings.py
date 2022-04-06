9d3aca57b754ee47a8ba1dbd3962a3da96d9a965
Chris Chambers <magma.chambers@gmail.com>
2022-04-06 23:48:17 +0100

    Fix ipython configuration failing in Emacs REPL

--- private_dot_ipython/profile_default/startup/keybindings.py
+++ private_dot_ipython/profile_default/startup/keybindings.py
@@ -7,29 +7,34 @@ from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.filters import HasFocus, ViInsertMode, ViNavigationMode
 from prompt_toolkit.key_binding.bindings.named_commands import get_by_name
 
+def apply_keybindings(registry):
+
+    ph = get_by_name('previous-history')
+    registry.add_binding('K',
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViNavigationMode()))(ph)
+
+    nh = get_by_name('next-history')
+    registry.add_binding('J',
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViNavigationMode()))(nh)
+
+    # Add classic Emacs-style shell keybindings to insert mode:
+    registry.add_binding("c-a",
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViInsertMode()))(get_by_name("beginning-of-line"))
+    registry.add_binding("c-e",
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViInsertMode()))(get_by_name("end-of-line"))
+    registry.add_binding("c-w",
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViInsertMode()))(get_by_name("backward-kill-word"))
+    registry.add_binding("c-k",
+                 filter=(HasFocus(DEFAULT_BUFFER) &
+                     ViInsertMode()))(get_by_name("kill-line"))
+
 ip = get_ipython()
-registry = ip.pt_app.key_bindings
-
-
-ph = get_by_name('previous-history')
-registry.add_binding('K',
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViNavigationMode()))(ph)
-nh = get_by_name('next-history')
-registry.add_binding('J',
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViNavigationMode()))(nh)
-
-# Add classic Emacs-style shell keybindings to insert mode:
-registry.add_binding("c-a",
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViInsertMode()))(get_by_name("beginning-of-line"))
-registry.add_binding("c-e",
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViInsertMode()))(get_by_name("end-of-line"))
-registry.add_binding("c-w",
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViInsertMode()))(get_by_name("backward-kill-word"))
-registry.add_binding("c-k",
-		     filter=(HasFocus(DEFAULT_BUFFER) &
-			     ViInsertMode()))(get_by_name("kill-line"))
+# This prevents REPLs within emacs from having a fit:
+if ip.pt_app:
+    registry = ip.pt_app.key_bindings
+    apply_keybindings(registry)
